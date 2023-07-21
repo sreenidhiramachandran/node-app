99d9b78a5f17743c4506e4a0387121e47757dc26
Sreenidhi Ramachandran <120683482+sreenidhiramachandran@users.noreply.github.com>
2023-07-21 14:28:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Sample Node App Version 2\n');
+  res.send('Sample Node App Version 3\n');
 });
 
 app.listen(PORT, HOST);
